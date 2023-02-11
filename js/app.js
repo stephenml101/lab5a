371a7068f303b90485778cae8ac30b0961ad8728
Stephen <stephenlevesque2@gmail.com>
2023-02-10 18:37:06 -0700

    problem two branch

--- js/app.js
+++ js/app.js
@@ -29,12 +29,18 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
+function multiply(a, b)
+
+{ //eslint-disable-line
+  let myMultiply = a*b;
+  let myStr = `The product of ${a} and ${b} is ${myMultiply}.`;
+
+  return [myMultiply,myStr];
 
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
